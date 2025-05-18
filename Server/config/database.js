21e97296c8cd9994cd711e400dbbc4c27bdf497a
priyanshu123-ai <priyanshukumarpriyanshu015@gmail.com>
2025-05-18 11:07:41 +0530

    Edtech_Project

---
+++ Server/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL)
+        .then(() => console.log("DB Connected Successfully"))
+        .catch((error) => {
+            console.log("DB Connection Failed:", error);
+            process.exit(1);
+        });
+};
